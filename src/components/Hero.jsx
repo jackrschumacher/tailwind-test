ef3acd44b1719b097872021b44d1466788e9e676
Jack Schumacher <jackrschumacher@gmail.com>
2025-04-12 23:08:31 -0500

    Update styles to test

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,10 +15,10 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915EFF]">lohit</span>
+            Hi, I'm <span className="text-[green]">Jack</span>
           </h1>
-          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I do
+          <p className={`${styles.heroSubText} mt-2 text-blue-100`}>
+            This site is currently a work in progress
             <Typewriter
               options={{
                 strings: ["CyberSecurity", "Ai & ML", "NLP"],
